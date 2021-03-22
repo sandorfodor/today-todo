a459591b949472d552b58e122a20cffeab6db2be
Sandor Fodor <hccoder@gmail.com>
2021-03-22 14:57:50 +0100

    Add e2e like test for app(add item to the empty list)

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 
@@ -14,6 +14,7 @@ describe('<App />', () => {
       { name: 'test item 1' },
       { name: 'test item 2' }
     ]);
+    const getItemOriginalImplementation = Storage.prototype.getItem;
     Storage.prototype.getItem = jest.fn(() => storageValue);
 
     const container = render(<App />);
@@ -23,5 +24,27 @@ describe('<App />', () => {
 
     const secondItem = screen.queryByText(/test item 2/i);
     expect(secondItem).toBeInTheDocument();
+
+    Storage.prototype.getItem = getItemOriginalImplementation;
+  });
+
+  it('add todo item to the empty list', () => {
+    const container = render(<App />);
+    const emptyStateText = screen.getByText(/nothing to do/i);
+    expect(emptyStateText).toBeInTheDocument();
+
+    const input = container.getByLabelText('Name of the todo');
+    fireEvent.change(input, { target: { value: 'test item 1' } })
+
+    const submitButton = container.getByText('Add todo');
+    fireEvent(submitButton,
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+      })
+    );
+
+    const todoItem = screen.queryByText(/test item 1/i);
+    expect(todoItem).toBeInTheDocument();
   });
 });
